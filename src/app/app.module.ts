dd52823b0ad16e434b2fa2b27de236cf1508c29a
Shreyas <shreyassomani27@gmail.com>
2019-09-22 00:20:48 +0530

    Task Manager-Angular Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddtaskComponent } from './addtask/addtask.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ViewtaskComponent } from './viewtask/viewtask.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddtaskComponent,
+    NavbarComponent,
+    ViewtaskComponent
   ],
   imports: [
     BrowserModule
